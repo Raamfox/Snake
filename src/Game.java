6b6b4d14d5cf11b250354013e10ca7720f2cc5f4
Raamfox <rammiokal@hotmail.com>
2021-10-22 21:20:49 +0200

    Created the dimensions for Jframe

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,23 @@
+import javax.swing.*;
+
 public class Game {
 
+    private Snake player;
+    private Food food;
+    private Graphics graphics;
+
+    private JFrame window;
+
+    public static final int width = 20;
+    public static final int height = 20;
+    public static final int dimension = 20;
+
     public Game() {
-        //add game code here
-        //poop
-        
+        window = new JFrame();
+
+        window.setTitle("Snake");
+        window.setSize(width * dimension, height * dimension);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
